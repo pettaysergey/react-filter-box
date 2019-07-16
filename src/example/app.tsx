741f1fcda860ff305c20b45a110f7f00b171c4d4
pettay sergey <spettay@smedialink.com>
2019-07-16 10:14:35 +0300

    formating

--- src/example/app.tsx
+++ src/example/app.tsx
@@ -6,8 +6,8 @@ import "fixed-data-table/dist/fixed-data-table.min.css";
 import "../ReactFilterBox.less";
 import "./app.less";
 import Demo1 from "./demo1";
-// import Demo2 from "./demo2";
-// import Demo3 from "./demo3";
+import Demo2 from "./demo2";
+import Demo3 from "./demo3";
 
 import { hot } from "react-hot-loader";
 
@@ -17,8 +17,8 @@ class App extends React.Component<any, any> {
       <div>
         <h2 style={{ textAlign: "center" }}>React Filter Box</h2>
         <Demo1 />
-        {/* <Demo2 />
-                <Demo3 /> */}
+        <Demo2 />
+        <Demo3 />
       </div>
     );
   }
